39afa37819a70595e2a4db2f964fec5bbbb4f2dc
Vinicius Paes Berna <vini_berna2.0@hotmail.com>
2024-04-29 11:59:01 -0300

    chore: add eslint and prettier rules

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,28 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import './globals.css'
 
-const inter = Inter({ subsets: ["latin"] });
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import { ReactNode } from 'react'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: {
-    template: "%s | Halo Company",
-    default: "Halo Company",
+    template: '%s | Halo Company',
+    default: 'Halo Company',
   },
-  description: "Your application to store and manage files with your team or by yourself.",
-};
+  description:
+    'Your application to store and manage files with your team or by yourself.',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: ReactNode
 }>) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  );
+  )
 }
